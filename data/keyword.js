14f813f846fe2ea99261444a38b5b27fdad21d47
Ethan Silverstein <ethanlsilverstein@gmail.com>
2024-03-27 20:26:08 -0400

    Add functions for keywords and posts

--- data/keyword.js
+++ data/keyword.js
@@ -9,3 +9,82 @@ import * as helper from "../helpers.js"
   "posts": ["ObjectId (Posts)"]
 }
  */
+
+/**
+ * Creates a new keyword in the database.
+ * @param {string} keyword - The keyword to be created.
+ * @returns {number} - Returns 0 if the keyword is successfully created, otherwise returns 1.
+ */
+const create = async (
+    keyword,
+) => {
+    if (helper.areAllValuesNotNull([keyword])) {
+        return 1;
+    }
+
+    if (!helper.areAllValuesOfType([keyword], 'string')) {
+        return 1
+    }
+
+    keyword = keyword.trim();
+
+    const keywordCollection = await keywords();
+    const newKeyword = {
+        keyword,
+        posts: []
+    };
+
+    const insertInfo = await keywordCollection.insertOne(newKeyword);
+    if (insertInfo.insertedCount === 0) {
+        return 1;
+    }
+
+    return 0;
+}
+
+const getKeywordById = async (id) => {
+    if (helper.isNull(id)) {
+        return null;
+    }
+
+    if (!helper.isOfType(id, 'string')) {
+        return null;
+    }
+
+    id = id.trim();
+
+    const keywordCollection = await keywords();
+    const keywordObj = await keywordCollection.findOne({ _id: id });
+    if (helper.isNull(keywordObj)) {
+        return null;
+    }
+
+    return keywordObj;
+}
+
+/**
+ * Deletes a keyword from the database.
+ * @param {string} keyword - The keyword to be deleted.
+ * @returns {number} - Returns 0 if the keyword is successfully deleted, otherwise returns 1.
+ */
+const deleteKeyword = async (keyword) => {
+    if (helper.areAllValuesNotNull([keyword])) {
+        return 1;
+    }
+
+    if (!helper.areAllValuesOfType([keyword], 'string')) {
+        return 1;
+    }
+
+    keyword = keyword.trim();
+
+    // TODO make sure we delete the keyword from all posts before deleting the keyword
+
+    const keywordCollection = await keywords();
+    const deletionInfo = await keywordCollection.deleteOne({ keyword });
+    if (deletionInfo.deletedCount === 0) {
+        return 1;
+    }
+
+    return 0;
+}
--- data/posts.js
+++ data/posts.js
@@ -22,3 +22,104 @@ import * as helper from "../helpers.js"
   "createdAt": "Date"
 }
  */
+
+/**
+ * Creates a new post in the database.
+ * @param {string} user - The user that created the post.
+ * @param {string} image - The image URL of the post.
+ * @param {Array<string>} clothingLinks - The links to the clothing items in the image.
+ * @param {Array<string>} keywords - The keywords associated with the post.
+ * @param {Array<string>} likes - The users that liked the post.
+ * @param {Array<string>} comments - The comments associated with the post.
+ * @param {Date} createdAt - The date the post was created.
+ * @returns {number} - Returns 0 if the post is successfully created, otherwise returns 1.
+ */
+const create = async (
+    user,
+    image,
+    clothingLinks,
+    keywords
+) => {
+    if (helper.areAllValuesNotNull([user, image, clothingLinks, keywords])) {
+        return 1;
+    }
+
+    if (!helper.areAllValuesOfType([user, image], 'string')) {
+        return 1
+    }
+
+    user = user.trim();
+    image = image.trim();
+
+    if (!helper.isArrayOfType(clothingLinks, 'string')) {
+        return 1;
+    }
+
+    if (!helper.isArrayOfType(keywords, 'string')) {
+        return 1;
+    }
+
+    // Check if the user exists
+    const userCollection = await users();
+    const userObj = await userCollection.findOne({ _id: user });
+    if (helper.isNull(userObj)) {
+        return 1;
+    }
+
+    // Check if the keywords exist if they do not exist create them
+    const keywordCollection = await keywords();
+    for (let i = 0; i < keywords.length; i++) {
+        const keywordObj = await keywordCollection.findOne({ keyword: keywords[i] });
+        if (helper.isNull(keywordObj)) {
+            const keyword = await create(keywords[i]);
+            if (keyword === 1) {
+                return 1;
+            }
+        }
+    }
+
+    // TODO Check that the image and clothing URLs are valid
+
+    const postCollection = await posts();
+    const newPost = {
+        user,
+        image,
+        clothingLinks,
+        keywords,
+        likes: [],
+        comments: [],
+        createdAt: new Date()
+    };
+
+    const insertInfo = await postCollection.insertOne(newPost);
+    if (insertInfo.insertedCount === 0) {
+        return 1;
+    }
+
+    return 0;
+}
+
+/**
+ * Gets a post by its ID.
+ * @param {string} id - The ID of the post.
+ * @returns {object} - Returns the post if it exists, otherwise returns null.
+ */
+const getPostById = async (id) => {
+    if (helper.isNull(id)) {
+        return null;
+    }
+
+    if (!helper.isOfType(id, 'string')) {
+        return null;
+    }
+
+    id = id.trim();
+
+    const postCollection = await posts();
+    const post = await postCollection.findOne({ _id: id });
+    if (helper.isNull(post)) {
+        return null;
+    }
+
+    return post;
+}
